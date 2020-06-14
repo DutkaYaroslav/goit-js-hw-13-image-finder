5ffabb1c0e35756a1e966642bd3c731a2f328ee3
unknown <yaroslavdutka@gmail.com>
2020-06-14 21:03:43 +0300

    3

--- src/index.js
+++ src/index.js
@@ -2,27 +2,33 @@ import './styles.css';
 import api from './apiService.js';
 import dElements from './template/result.hbs';
 
-import { alert, defaultModules } from '@pnotify/core';
+console.log(dElements)
+
+import {
+  alert,
+  defaultModules
+} from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import * as PNotifyMobile from '@pnotify/mobile';
 import '@pnotify/mobile/dist/PNotifyMobile.css';
 
 const refs = {
-  input: document.querySelector('.input'),
+  input: document.querySelector('.search-form'),
   ulResult: document.querySelector('.gallery'),
   items: document.querySelector('.photo-card'),
 };
 
-const debounce = require('lodash.debounce');
 
-refs.input.addEventListener('input', debounce(inputLogic, 500));
 
 function inputLogic(e) {
   refs.ulResult.innerHTML = '';
   if (e.target.value) {
-    api(refs.input.value, pageIcrement, getData);
+    api(refs.input.value, 1, getData);
   }
 }
+
+refs.input.addEventListener('submit', inputLogic);
+
 
 function getData(data) {
   if (data.length > 0) {
@@ -33,22 +39,26 @@ function getData(data) {
 }
 
 function showPhotos(array) {
-  const html = dElements(array);
+  const html = array.map(arr => dElements(arr)).join('');
 
-  refs.items.insertAdjacentHTML('beforeend', html);
+  refs.ulResult.insertAdjacentHTML('beforeend', html);
 }
 
-function pageIcrement() {
-  let pageNumber = 1;
-  const step = 1;
 
-  if (aaa) {
-    pageNumber += step;
-  }
-}
+// function pageIncrement() {
+//   let pageNumber = 1;
+//   const step = 1;
+
+
+//   pageNumber += step;
+
+// }
 
 function onError(notification) {
   defaultModules.set(PNotifyMobile, {});
 
   alert('try something else');
 }
+
+
+console.log(inputLogic())
