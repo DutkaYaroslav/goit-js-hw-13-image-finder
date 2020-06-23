36802f810275e4df608463b30d9fa6fdba09d946
unknown <yaroslavdutka@gmail.com>
2020-06-23 11:13:25 +0300

    hello

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import './styles.css';
 import api from './apiService.js';
 import dElements from './template/result.hbs';
 
-console.log(dElements)
-
 import {
   alert,
   defaultModules
@@ -13,26 +11,47 @@ import * as PNotifyMobile from '@pnotify/mobile';
 import '@pnotify/mobile/dist/PNotifyMobile.css';
 
 const refs = {
-  input: document.querySelector('.search-form'),
+  input: document.querySelector('#search-form'),
+  // inputInput: document.querySelector('.input'),
   ulResult: document.querySelector('.gallery'),
   items: document.querySelector('.photo-card'),
+  loadMore: document.querySelector('.load-more_button'),
+  image: document.querySelector('.small-image'),
+  isOpen: document.querySelector('.js-lightbox'),
+
 };
-
-
-
-function inputLogic(e) {
-  refs.ulResult.innerHTML = '';
-  if (e.target.value) {
-    api(refs.input.value, 1, getData);
-  }
-}
 
 refs.input.addEventListener('submit', inputLogic);
 
+let page = 1;
+
+function inputLogic(e) {
+  page = 1;
+  e.preventDefault();
+  refs.ulResult.innerHTML = '';
+  const form = e.currentTarget;
+  const input = form.elements.query;
+
+  if (input.value) {
+    api(input.value, page, getData);
+  }
+}
+
+refs.loadMore.addEventListener('click', pageNumber);
+
+function pageNumber(e) {
+  if (refs.loadMore) {
+    page += 1;
+
+    const form = refs.input;
+    const input = form.elements.query;
+    api(input.value, page, getData);
+  }
+}
 
 function getData(data) {
-  if (data.length > 0) {
-    showPhotos(data);
+  if (data.hits.length > 0) {
+    showPhotos(data.hits);
   } else {
     onError();
   }
@@ -44,21 +63,31 @@ function showPhotos(array) {
   refs.ulResult.insertAdjacentHTML('beforeend', html);
 }
 
-
-// function pageIncrement() {
-//   let pageNumber = 1;
-//   const step = 1;
-
-
-//   pageNumber += step;
-
-// }
-
 function onError(notification) {
   defaultModules.set(PNotifyMobile, {});
 
   alert('try something else');
 }
 
+// if (refs.input) {
+//   refs.loadMore.elements;
+// }
 
-console.log(inputLogic())
+
+
+
+
+
+function bigImage(e) {
+  if (e.target === e.currentTarget) {
+    return;
+  }
+  // e.preventDefault();
+  refs.isOpen.classList.add('is-open');
+  refs.image.src = e.target.largeImageURL;
+  // refs.button.classList.add('lightbox__button');
+
+  // console.log(e)
+}
+
+refs.ulResult.addEventListener('click', bigImage);
